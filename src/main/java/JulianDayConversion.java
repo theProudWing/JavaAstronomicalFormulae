766392edb4066adfb566c58adba7c604ac581af6
theProudWing <80337143+theProudWing@users.noreply.github.com>
2026-07-08 17:20:20 +0100

    [Feature] Added function to convert Julian Day values into LocalDate values

--- src/main/java/JulianDayConversion.java
+++ src/main/java/JulianDayConversion.java
@@ -13,42 +13,66 @@ public class JulianDayConversion {
     public static void main(String[] args){
         JulianDayConversion converter = new JulianDayConversion();
 
+        /*
         double millenniumJD = converter.calculateJulianDay( LocalDate.of(2000, 1, 1));
         System.out.println("Millennium: " + millenniumJD);
 
         double currentJD = converter.calculateJulianDay(LocalDate.now());
         System.out.println("Current: " + currentJD);
+        */
 
+        //Example 3.a
         double sputnik1JD = converter.calculateJulianDay(1957, 10, 4.81);
-        System.out.println("Launch of Sputnik 1: " + sputnik1JD);
+
+
+        //Example 3.b
+        double example3BJD = converter.calculateJulianDay(333, 1, 27.5);
+        System.out.println("January 27, 333 @ 12:00 : " + example3BJD);
+
+        //Example of negative years - Working!
+        double negativeJD = converter.calculateJulianDay(-584, 5, 28.63);
+        System.out.println("May 28.63, -584 : " + negativeJD);
+
+        //Converting Back from Julian Day
+        LocalDate sputnikLaunch = converter.calculateDateFromJulian(sputnik1JD);
+        System.out.println("Launch of Sputnik 1: " + sputnikLaunch.toString());
+
+        // Exercise - Calculate the calendar dates of JD = 1842713.0, AND JD = 1507900.13
+        LocalDate answer1 = converter.calculateDateFromJulian(1842713);
+        System.out.println("Answer 1: " + answer1.toString());
+        LocalDate answer2 = converter.calculateDateFromJulian(1507900.13);
+        System.out.println("Answer 2: " + answer2.toString());
+
     }
 
-    public double calculateJulianDay(final int YEAR, final int MONTH, final double DAY_OF_MONTH){
-        // Create a String with the format "YYYY.MMDD" that can then be converted to a double
-        String dateString = YEAR + ".";
-        if (MONTH < 10){
+    public double calculateJulianDay(final int year, final int month, final double dayOfMonth){
+        //TODO: Throw an exception if provided date is outside of the calculable range
+
+        // Create a String with the format "YYYY.MMDDdd"
+        String dateString = year + ".";
+        if (month < 10){
             dateString += "0";      // MM must be 2 digits
         }
-        dateString += MONTH;
-        if (DAY_OF_MONTH < 10){
-            dateString += "0";      // DD must be 2 digits
+        dateString += month;
+        if (dayOfMonth < 10){
+            dateString += "0";      // DDdd must be at least 2 digits
         }
-        dateString += Double.toString(DAY_OF_MONTH).replace(".", ""); // remove . from double
+        dateString += Double.toString(dayOfMonth).replace(".", ""); // remove any '.' from double
 
         double dateDouble = Double.parseDouble(dateString); // Used later to check whether date is in the Gregorian Calendar
 
         int y, m, A, B;
 
-        if (MONTH > 2){
-            y = YEAR;
-            m = MONTH;
+        if (month > 2){
+            y = year;
+            m = month;
         } else {
-            y = YEAR - 1;
-            m = MONTH + 12;
+            y = year - 1;
+            m = month + 12;
         }
 
         // Base Calculation for determining Julian day
-        double JD = ((int)(365.25 * y) + (int)(30.6001 * (m + 1))  + DAY_OF_MONTH + 1_720_994.5);
+        double JD = ((int)(365.25 * y) + (int)(30.6001 * (m + 1))  + dayOfMonth + 1_720_994.5);
 
         // Check whether date is in Gregorian Calendar
         if (dateDouble >= 1582.1015){
@@ -66,5 +90,32 @@ public class JulianDayConversion {
         final int DAY_OF_MONTH = date.getDayOfMonth();
 
         return calculateJulianDay(YEAR, MONTH, DAY_OF_MONTH);
+    }
+
+    public LocalDate calculateDateFromJulian(double julianDay){
+        double temp = julianDay + 0.5;
+        String tempString = Double.toString(temp);
+
+        int Z = (int) temp;
+        double F = Double.parseDouble("0." + tempString.split("\\.")[1]);
+
+        int A;
+
+        if (Z >= 2_299_161){
+            int alpha = (int)((Z - 1_867_216.25) / 36524.25);
+            A = (Z + 1 + alpha) - (alpha / 4);
+        } else {
+            A = Z;
+        }
+        int B = A + 1524;
+        int C = (int)((B - 122.1) / 365.25);
+        int D = (int)(365.25 * C);
+        int E = (int)((B-D) / 30.6001);
+
+        double dayOfMonth = B - D - (int)(30.6001 * E) + F;
+        int month = E < 13.5 ? E - 1 : E - 13;
+        int year = month > 2 ? C - 4716 : C - 4715;
+
+        return LocalDate.of(year, month, (int)dayOfMonth);
     }
 }
